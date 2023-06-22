9140d292e0761fe0380a1e1407cf6bc2bb1ca408
Gabriela <gabriela.mmorgado@gmail.com>
2023-06-21 21:27:31 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// 3. Faça um programa que recebe um número inteiro e mostre a contagem de 1 até tal número.
+
+const target = window.prompt('Digite um número inteiro:');
+
+for (let i=1; i<=target; i++) {
+    console.log(i);
+}
